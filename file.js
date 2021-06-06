26711073b7b7b49f536ac1034223bf2d7f2236af
Atmanand Nagpure <skillzworldtech@gmail.com>
2021-06-06 20:31:11 +0530

    Added exception handling to readLines() function

--- file.js
+++ file.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
 
 const readLines = (__filepath) => {
-  let array = fs.readFileSync(__filepath).toString().split('\n');
-  return array;
+  try {
+    let array = fs.readFileSync(__filepath).toString().split('\n');
+    return array;
+  } catch (e) {
+    console.error(`Error occured : Cannot read file ${__filepath}`);
+    return [];
+  }
 };
 
 module.exports = {
